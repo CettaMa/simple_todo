05dfc7d4398e274d9edce64b9b4955c09f8ca72a
CettaMa <93418128+CettaMa@users.noreply.github.com>
2025-01-01 13:24:32 +0700

    rewrite the app

--- app.py
+++ app.py
@@ -1,50 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
-
-def __repr__(self):
-        return f"<Task {self.id} - {self.content}>"
+todo_list = []  # Simple in-memory list to store tasks
 
 @app.route('/')
 def index():
-    tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks)
+    return render_template('index.html', tasks=todo_list)
 
 @app.route('/add', methods=['POST'])
-def add_task():
-    task_content = request.form.get('content')
-    if task_content:
-        new_task = Task(content=task_content)
-        db.session.add(new_task)
-        db.session.commit()
+def add():
+    task = request.form.get('task')
+    if task:
+        todo_list.append(task)
     return redirect(url_for('index'))
 
-@app.route('/delete/<int:task_id>')
-def delete_task(task_id):
-    task = Task.query.get(task_id)
-    if task:
-        db.session.delete(task)
-        db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/complete/<int:task_id>')
-def complete_task(task_id):
-    task = Task.query.get(task_id)
-    if task:
-        task.completed = not task.completed
-        db.session.commit()
+@app.route('/delete/<int:task_id>', methods=['POST'])
+def delete(task_id):
+    if 0 <= task_id < len(todo_list):
+        todo_list.pop(task_id)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
