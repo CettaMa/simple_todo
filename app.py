f47b345730abd56bd63acf2295768c82269ea9c4
Ilhammkt <93485872+Ilhammkt@users.noreply.github.com>
2025-01-01 16:19:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
 
 # Separate lists for ongoing and completed tasks
 ongoing_tasks = []
