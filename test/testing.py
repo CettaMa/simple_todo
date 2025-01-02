cdec295ed9066a6cbb9af19da9742fa8578c56e8
CettaMa <93418128+CettaMa@users.noreply.github.com>
2025-01-02 15:27:07 +0700

    remove unused import of time in testing.py

--- test/testing.py
+++ test/testing.py
@@ -3,7 +3,6 @@ import sys
 import os
 import logging
 from flask import session
-import time
 
 # Add the parent directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
